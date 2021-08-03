e6e0bd4b950561e42d2dc99b52823e3a5dec9c50
Crowdbotics <team@crowdbotics.com>
2021-08-03 14:59:21 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "black_hall_29401Identifier",
+  url: "https://black_hall_29401Identifier.botics.co",
+  api: "https://black_hall_29401Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
